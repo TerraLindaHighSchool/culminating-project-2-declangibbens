230b8b8a13c8b4a927560e24ca5548d7cc3f43c9
declangibbens <60672327+declangibbens@users.noreply.github.com>
2021-01-07 15:15:37 -0800

    Milestone 1.3 Completed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public bool isGameActive = true;
+    private float xMin = -68.5f;
+    private float xMax = -45.0f;
+    private float ySpawnPos = -21.25f;
+    private float vehiclePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vehiclePos = GameObject.Find("Vehicle").transform.position.z;
+        
+        
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index], RandomSpawnPos(), transform.rotation);
+        }
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(xMin, xMax), ySpawnPos, vehiclePos + 165);
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float currentPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPos = transform.position.y;
+        if (currentPos < -30)
+        {
+            Destroy(gameObject);
+            Debug.Log("HI");
+        }
+    }
+}
